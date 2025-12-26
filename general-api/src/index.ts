491775d430e47801c0f434714b533f6a6c768983
Anton Erne <antonerne@yahoo.com>
2025-12-26 18:41:21 +0900

    Dec26-1841

--- general-api/src/index.ts
+++ general-api/src/index.ts
@@ -4,8 +4,9 @@ import app from './app';
 
 dotenv.config();
 
+const now = new Date();
 const logger = new Logger(
-  `${process.env.LOG_DIR}/general/process_${(new Date().toDateString())}.log`);
+  `${process.env.LOG_DIR}/general/process_${now.getMonth()}-${now.getFullYear()}.log`);
 const PORT = process.env.PORT || 7005;
 app.listen(PORT, () => {
   logger.log(`Server is running on port ${PORT}`);
--- general-api/src/routes/logsRoutes.ts
+++ general-api/src/routes/logsRoutes.ts
@@ -1,6 +1,7 @@
 import { Request, Response, Router } from "express";
-import { Log, LogList } from "scheduler-node-models/general";
+import { ILogEntry, Log, LogEntry, LogList } from "scheduler-node-models/general";
 import * as fs from 'fs';
+import * as path from 'path';
 
 const router = Router();
 
@@ -16,9 +17,16 @@ router.get('/logs', async(req: Request, res: Response) => {
       const dirents = await fs.promises.readdir(logDir, { withFileTypes: true })
       const directories = dirents.filter(dirent => dirent.isDirectory())
         .map(dirent => dirent.name);
-      directories.forEach(dir => {
+      directories.forEach(async(dir) => {
         const log = new Log();
         log.name = dir;
+        const entries = await getEntriesFromLogs(log.name);
+        if (entries.length > 0) {
+          entries.forEach(entry => {
+            log.entries.push(new LogEntry(entry));
+          });
+          log.entries.sort((a,b) => b.compareTo(a));
+        }
         list.logs.push(log)
       });
       list.logs.sort((a,b) => a.compareTo(b));
@@ -29,5 +37,42 @@ router.get('/logs', async(req: Request, res: Response) => {
     res.status(400).json({'message': error.message});
   }
 });
+
+const getEntriesFromLogs = async (logDir: string): Promise<ILogEntry[]> => {
+  const list: LogEntry[] = [];
+  try {
+    const directoryPath = path.join(`${process.env.LOG_DIR}`, logDir);
+    const fileEnts = await fs.promises.readdir(directoryPath, { withFileTypes: true });
+    const files = fileEnts.filter(entry => entry.isFile())
+      .map(entry => path.join(directoryPath, entry.name));
+    files.forEach(async(filePath) => {
+      let contents: string = '';
+      await fs.readFile(filePath, (err, data) => {
+        if (err) {
+          throw err;
+        }
+        contents = data.toString();
+      });
+      const rows = contents.split('\n');
+      rows.forEach(row => {
+        const data = row.split('\t');
+        if (data.length > 1) {
+          list.push(new LogEntry({
+            date: new Date(Number(data[0])),
+            entry: data[1]
+          }));
+        } else if (data.length === 1) {
+          list.push(new LogEntry({
+            date: new Date(0),
+            entry: data[0]
+          }));
+        }
+      })
+    });
+  } catch (err) {
+    throw err;
+  }
+  return list;
+};
 
 export default router;
