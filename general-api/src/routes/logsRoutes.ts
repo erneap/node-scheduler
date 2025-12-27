f94edf7eced7f9ccd7546ee25024734aef470dbd
Anton Erne <antonerne@yahoo.com>
2025-12-27 09:21:31 +0900

    Dec27-0921

--- general-api/src/routes/logsRoutes.ts
+++ general-api/src/routes/logsRoutes.ts
@@ -1,9 +1,11 @@
 import { Request, Response, Router } from "express";
-import { ILogEntry, Log, LogEntry, LogList } from "scheduler-node-models/general";
+import { ILogEntry, Log, LogEntry, Logger, LogList } from "scheduler-node-models/general";
 import * as fs from 'fs';
 import * as path from 'path';
 
 const router = Router();
+const logger = new Logger(
+  `${process.env.LOG_DIR}/general/process_${(new Date().toDateString())}.log`);
 
 /**
  * This web api method will provide a list of available logs as the sub-directories
@@ -14,13 +16,13 @@ router.get('/logs', async(req: Request, res: Response) => {
     const list = new LogList();
     const logDir = (process.env.LOG_DIR) ? process.env.LOG_DIR : '';
     if (logDir !== '') {
-      const dirents = await fs.promises.readdir(logDir, { withFileTypes: true })
+      const dirents = fs.readdirSync(logDir, { withFileTypes: true })
       const directories = dirents.filter(dirent => dirent.isDirectory())
         .map(dirent => dirent.name);
-      directories.forEach(async(dir) => {
+      directories.forEach(dir => {
         const log = new Log();
         log.name = dir;
-        const entries = await getEntriesFromLogs(log.name);
+        const entries = getEntriesFromLogs(log.name);
         if (entries.length > 0) {
           entries.forEach(entry => {
             log.entries.push(new LogEntry(entry));
@@ -34,38 +36,32 @@ router.get('/logs', async(req: Request, res: Response) => {
     }
   } catch (err) {
     const error = err as Error;
+    logger.log(`Error: ${error.message}`);
     res.status(400).json({'message': error.message});
   }
 });
 
-const getEntriesFromLogs = async (logDir: string): Promise<ILogEntry[]> => {
+const getEntriesFromLogs = (logDir: string): ILogEntry[] => {
   const list: LogEntry[] = [];
+  const days30 = new Date((new Date()).getTime() - (30 * 24 * 3600000));
   try {
     const directoryPath = path.join(`${process.env.LOG_DIR}`, logDir);
-    const fileEnts = await fs.promises.readdir(directoryPath, { withFileTypes: true });
+    const fileEnts = fs.readdirSync(directoryPath, { withFileTypes: true });
     const files = fileEnts.filter(entry => entry.isFile())
       .map(entry => path.join(directoryPath, entry.name));
-    files.forEach(async(filePath) => {
-      let contents: string = '';
-      await fs.readFile(filePath, (err, data) => {
-        if (err) {
-          throw err;
-        }
-        contents = data.toString();
-      });
+    files.forEach(filePath => {
+      let contents: string = fs.readFileSync(filePath, 'utf-8');
       const rows = contents.split('\n');
       rows.forEach(row => {
         const data = row.split('\t');
         if (data.length > 1) {
-          list.push(new LogEntry({
-            date: new Date(Number(data[0])),
-            entry: data[1]
-          }));
-        } else if (data.length === 1) {
-          list.push(new LogEntry({
-            date: new Date(0),
-            entry: data[0]
-          }));
+          const entryDate = new Date(Number(data[0]));
+          if (entryDate.getTime() > days30.getTime()) {
+            list.push(new LogEntry({
+              date: new Date(Number(data[0])),
+              entry: data[1]
+            }));
+          }
         }
       })
     });
