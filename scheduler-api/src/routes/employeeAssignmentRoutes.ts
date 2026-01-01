9280c752397a5de0e0bc3b09e96ecfc14a88a25f
Anton Erne <antonerne@yahoo.com>
2026-01-01 18:28:39 +0900

    Jan01-1828

--- scheduler-api/src/routes/employeeAssignmentRoutes.ts
+++ scheduler-api/src/routes/employeeAssignmentRoutes.ts
@@ -1,11 +1,25 @@
 import { Request, Response, Router } from "express";
 import { auth } from '../middleware/authorization.middleware';
 import { logConnection } from "../config/logging";
+import { NewEmployeeAssignment } from "scheduler-node-models/scheduler/employees";
 
 const router = Router();
 
 router.post('/employee/assignment', async(req: Request, res: Response) => {
+  try {
+    const data = req.body as NewEmployeeAssignment;
+    if (data) {
 
+    } else {
+      throw new Error('No new employee assignment request data');
+    }
+  } catch (err) {
+    const error = err as Error;
+    if (logConnection.employeeLog) {
+      logConnection.employeeLog.log(`Error: GetEmployee: ${error.message}`);
+    }
+    res.status(400).json({'message': error.message});
+  }
 });
 
 export default router;
--- scheduler-node-models/src/scheduler/employees/web.ts
+++ scheduler-node-models/src/scheduler/employees/web.ts
@@ -5,3 +5,20 @@ export interface ChangeLeaveRequestResponse {
   leaverequest: LeaveRequest | undefined;
   error: Error | undefined;
 }
+
+export interface NewEmployeeAssignment {
+  employee: string;
+  site: string;
+  workcenter: string;
+  start: Date;
+  scheduledays: number;
+}
+
+export interface ChangeAssignment {
+  employee: string;
+  asgmt: number;
+  schedule?: number;
+  workday?: number;
+  field: string;
+  value: string;
+}
