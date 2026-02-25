20cab3b9408f9dec2b21dd69cf55b1a9b1fa6e88
Anton Erne <antonerne@yahoo.com>
2026-02-25 11:16:29 +0900

    Feb25-1116

--- scheduler-node-models/src/scheduler/teams/web.ts
+++ scheduler-node-models/src/scheduler/teams/web.ts
@@ -1,12 +1,45 @@
+import { HolidayType } from "./company";
+
 export interface NewTeam {
   id: string;
   name: string;
 }
 
 export interface NewCompany {
+  team: string;
   id: string;
   name: string;
   ingest: string;
   ingestPeriod?: number;
   startDay?: number;
+  ingestPwd?: string;
+}
+
+export interface NewCompanyHoliday {
+  team: string;
+  company: string;
+  name: string;
+  holidayType: HolidayType;
+  sort: number;
+}
+
+export interface NewModPeriod {
+  team: string;
+  companyid: string;
+  year: number;
+  start: Date;
+  end: Date;
+}
+
+export interface NewTeamSpecialtyContact {
+  team: string;
+  name: string;
+}
+
+export interface UpdateTeam {
+  team: string;
+  companyid?: string;
+  optid?: string;
+  field: string;
+  value: string;
 }
