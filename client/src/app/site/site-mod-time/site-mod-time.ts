8a94858a6a146ff5fa5dfefa19d9afe29f443f0a
Anton Erne <antonerne@yahoo.com>
2026-04-09 12:03:46 +0900

    Apr09-1203

--- client/src/app/site/site-mod-time/site-mod-time.ts
+++ client/src/app/site/site-mod-time/site-mod-time.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { EmployeeService } from '../../services/employee-service';
 import { SiteService } from '../../services/site-service';
 import { TeamService } from '../../services/team-service';
 import { Team } from 'scheduler-models/scheduler/teams';
+import { MonthPeriod, WeekPeriod } from './site-mod-time.models';
+import { Employee } from 'scheduler-models/scheduler/employees';
+import { Site } from 'scheduler-models/scheduler/sites';
 
 @Component({
   selector: 'app-site-mod-time',
@@ -11,6 +14,10 @@ import { Team } from 'scheduler-models/scheduler/teams';
   styleUrl: './site-mod-time.scss',
 })
 export class SiteModTime {
+  months = signal<MonthPeriod[]>([]);
+  employees = signal<Employee[]>([]);
+  start = signal<Date>(new Date());
+  end = signal<Date>(new Date());
 
   constructor(
     private empService: EmployeeService,
@@ -44,5 +51,48 @@ export class SiteModTime {
     while (modstart.getUTCDay() !== 6) {
       modstart = new Date(modstart.getTime() - (24 * 3600000));
     }
+    let start = new Date(modstart);
+    while (start.getUTCDay() !== 5) {
+      start = new Date(start.getTime() + (24 * 3600000));
+    }
+
+    const months: MonthPeriod[] = [];
+    let period: MonthPeriod | undefined;
+    while (start.getTime() <= modend.getTime() && start.getTime() <= now.getTime()) {
+      if (!period || (period && period.month.getTime() < modstart.getTime()) 
+        || (period && period.month.getUTCMonth() !== start.getUTCMonth())) {
+        period = new MonthPeriod();
+        period.month = new Date(start);
+        period.expand = (start.getUTCMonth() === now.getUTCMonth() 
+          && start.getUTCFullYear() === now.getUTCFullYear());
+        months.push(period);
+      }
+      const week = new WeekPeriod();
+      week.start = new Date(start.getTime() - (6 * 24 * 3600000));
+      week.end = new Date(start);
+      period.weeks.push(week);
+      period.weeks.sort((a,b) => a.compareTo(b));
+      start = new Date(start.getTime() + (7 * 24 * 3600000));
+    }
+    months.sort((a,b) => a.compareTo(b));
+    this.months.set(months);
+    this.start.set(modstart);
+    this.end.set(modend);
+  }
+
+  setEmployees() {
+    const eList: Employee[] = [];
+    const iSite = this.siteService.getSite();
+    if (iSite) {
+      const site = new Site(iSite);
+      if (site.employees) {
+        site.employees.forEach(emp => {
+          if (emp.hasModTime(this.start(), this.end())) {
+            eList.push(new Employee(emp));
+          }
+        });
+      }
+    }
+    this.employees.set(eList);
   }
 }
